74826e275c003701bc9a7cff3e13f18aa105b32b
bartekdziedzic <bartekkdziedzic@gmail.com>
2023-11-20 11:36:04 +0100

    added graph calculation method

--- src/main/java/com/example/airport/controller/DepartureController.java
+++ src/main/java/com/example/airport/controller/DepartureController.java
@@ -1,10 +1,14 @@
 package com.example.airport.controller;
 
 import com.example.airport.model.Departure;
+import com.example.airport.service.CalculationService;
 import com.example.airport.service.DepartureService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -12,16 +16,22 @@ import java.util.List;
 public class DepartureController {
 
     private final DepartureService departureService;
+    private final CalculationService calculationService;
 
-    public DepartureController(DepartureService departureService) {
+    public DepartureController(DepartureService departureService, CalculationService calculationService) {
         this.departureService = departureService;
+        this.calculationService = calculationService;
     }
 
+    @RequestMapping("/fly")
+    public List<Departure> getData() {
 
-//   @RequestMapping("/fly")
-//    public List<Departure> getData() {
-//
-//        return departureService.getDepartures();
-//    }
+        return departureService.getDepartures();
+    }
 
+    @GetMapping("/graph")
+    public HashMap<LocalDateTime, Integer> getGraph() {
+
+        return calculationService.calculateDepartureGraph(calculationService.getDepartures());
+    }
 }
--- src/main/java/com/example/airport/service/CalculationService.java
+++ src/main/java/com/example/airport/service/CalculationService.java
@@ -2,9 +2,11 @@ package com.example.airport.service;
 
 import com.example.airport.enums.AircraftCode;
 import com.example.airport.model.Departure;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
@@ -12,11 +14,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
+@Getter
 public class CalculationService {
 
     private DepartureService departureService;
     private final EnumMap<AircraftCode, Integer> aircraftCapacityMap;
-    private List<Departure> departures;
+    private final List<Departure> departures;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public CalculationService(DepartureService departureService) {
         this.departureService = departureService;
@@ -28,15 +32,18 @@ public class CalculationService {
     public final EnumMap<AircraftCode, Integer> getAircraftCapacityMap() {
 
         EnumMap<AircraftCode, Integer> enumMap = new EnumMap<AircraftCode, Integer>(AircraftCode.class);
+        enumMap.put(AircraftCode.A20N, 160);
         enumMap.put(AircraftCode.A320, 150);
         enumMap.put(AircraftCode.B38M, 186);
+        enumMap.put(AircraftCode.DH8D, 39);
+        enumMap.put(AircraftCode.E75L, 82);
         enumMap.put(AircraftCode.E190, 106);
         enumMap.put(AircraftCode.E195, 112);
 
         return enumMap;
     }
 
-    public void getGraphMap() { // based on api response timestamp extremes - create empty graph table ('formattedTime',Integer)
+    public HashMap<LocalDateTime, Integer> getGraphMap() { // based on api response timestamp extremes - create empty graph table ('formattedTime',Integer)
         LocalDateTime maxGraphTimestamp = getMaxTimestamp();
         LocalDateTime minTimestamp = getMinTimestamp();
         LocalDateTime minGraphTimestamp = getAdaptationDifferenceTimestamp(minTimestamp);
@@ -47,20 +54,26 @@ public class CalculationService {
             graphMap.put(iterationTimestamp, 0);
             iterationTimestamp = iterationTimestamp.plusMinutes(1);
         }
+        return graphMap;
     }
 
 
-    private void recalculateDepartureGraph(Departure departure) { //sum passenger Integer values corresponding to timestamp
+    public HashMap<LocalDateTime, Integer> calculateDepartureGraph(List<Departure> departures) { //sum passenger Integer values corresponding to timestamp
 
-        //use calculateSingleDeparture
-        //distribute passengers on main graph
-        //  map2.forEach((key, value) -> map1.merge(key, value, Integer::sum));
+        HashMap<LocalDateTime, Integer> graph = getGraphMap();
 
-
+        for (Departure departure : departures
+        ) {
+            // get distribution map for every departure
+            Map<LocalDateTime, Integer> singleDistribution = calculateSingleDeparture(departure);
+            // sum current map values with graph values
+            singleDistribution.forEach((key, value) -> graph.merge(key, value, Integer::sum));
+        }
+        return graph;
     }
 
 
-    private void calculateSingleDeparture(Departure departure) {
+    private Map<LocalDateTime, Integer> calculateSingleDeparture(Departure departure) {
 
         //use distribution adaptation method
         HashMap<LocalDateTime, Integer> adaptedDistributionMap = adaptDistribution(LocalDateTime.parse(departure.dep_time()));
@@ -74,7 +87,7 @@ public class CalculationService {
                         Map.Entry::getKey,
                         key -> key.getValue() * passengerAmount
                 ));
-
+        return dedicatedDistributionMap;
     }
 
 
@@ -121,14 +134,14 @@ public class CalculationService {
 
     private LocalDateTime getMaxTimestamp() { //get latest timestamp from response
         return departures.stream()
-                .map(departure -> LocalDateTime.parse(departure.dep_time()))
+                .map(departure -> LocalDateTime.parse(departure.dep_time(),formatter))
                 .max(LocalDateTime::compareTo)
                 .orElse(null);
     }
 
     private LocalDateTime getMinTimestamp() { //get earliest timestamp from response
         return departures.stream()
-                .map(departure -> LocalDateTime.parse(departure.dep_time()))              // replaced from .map(Departure::dep_time)
+                .map(departure -> LocalDateTime.parse(departure.dep_time(),formatter))              // replaced from .map(Departure::dep_time)
                 .min(LocalDateTime::compareTo)
                 .orElse(null);
     }
