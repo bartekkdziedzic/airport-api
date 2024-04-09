895ff4f1da3f7cd72943492db8be45bf1d83474d
bartekdziedzic <bartekkdziedzic@gmail.com>
2024-04-09 19:48:04 +0200

    - added moving between views - added airport iataCode text inputs on views

--- src/main/java/com/example/airport/controller/GraphController.java
+++ src/main/java/com/example/airport/controller/GraphController.java
@@ -3,22 +3,18 @@ package com.example.airport.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GraphController {
 
-    @GetMapping("/graph/{depIata}")
-    public String getGraph(@PathVariable("depIata") String depIata, Model model) {
-        model.addAttribute("city", depIata);
+    @GetMapping("/graph")
+    public String getGraph(Model model) {
         return "graph";
     }
 
-    @GetMapping("/frgraph/{depIata}")
-    public String getFrGraph(@PathVariable("depIata") String depIata, Model model) {
-        model.addAttribute("city", depIata);
+    @GetMapping("/frgraph")
+    public String getFrGraph(Model model) {
         return "frgraph";
     }
-
 
 }
